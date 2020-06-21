867c92082978f0c8d3cbb2c8eacb1c28749ff839
DanaAlhaqbani <Dana.hq9@gmail.com>
2020-06-21 09:09:16 +0300

    Initial Commit

---
+++ MC Mazaya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC Mazaya
+//
+//  Created by دانة الحقباني on 29/10/1441 AH.
+//  Copyright © 1441 MC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
