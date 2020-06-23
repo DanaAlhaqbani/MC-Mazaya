21fcc89eb16a7d6d71a25f1c1951ce50997609cf
DanaAlhaqbani <Dana.hq9@gmail.com>
2020-06-23 12:32:11 +0300

    Register + Reset password implementation

--- MC Mazaya/ResetPassViewController.swift
+++ MC Mazaya/ResetPassViewController.swift
@@ -7,27 +7,52 @@
 //
 
 import UIKit
-
+import FirebaseAuth
+import FirebaseDatabase
 class ResetPassViewController: UIViewController {
-
+  var resetFlag = false
     @IBOutlet weak var resetBtn: UIButton!
     @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         emailTextField.backgroundUnderlined()
         resetBtn.setButton()
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+    @IBAction func resetPassPressed(_ sender: UIButton) {
+        if emailTextField.text! == "" {
+                       //empty email
+                         let alert = self.alertContent(title:   "بياناتك غير مكتملة!", message: " من فضلِك ادخل بريدك الالكتروني" )
+                       self.present(alert, animated: true, completion: nil)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+                          }
+                   else {
+            let auth = Auth.auth()
+                       
+            auth.sendPasswordReset(withEmail: emailTextField.text!) { (error) in
+                           if let error = error {
+                             let alert = self.alertContent(title:   "البريد المدخل غير صحيح!", message: " من فضلِك ادخل  بريدك الالكتروني المسجل في حسابك" )
+                               self.present(alert, animated: true, completion: nil)
+                               return
+                           }
+                           
+                            let alert = self.alertContent(title:   "تم بنجاح!", message: " تم إرسال اليك بريد إلكتروني لإستعادة كلمة مرورك")
+                           self.present(alert, animated: true, completion: nil)
+                       }
+            
+        }
+ 
     }
-    */
-
+                                               
+       
+func alertContent(title: String, message: String)-> UIAlertController{
+    
+    let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    alertVC.addAction(UIAlertAction(title: NSLocalizedString("حسنًا", comment: "Default action"), style: .default, handler: { _ in
+        NSLog("The \"OK\" alert occured.")
+    }))
+    return alertVC
+}
 }
